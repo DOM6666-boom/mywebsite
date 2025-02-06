31d513efd44dc56377e3b7d82ddbee4443ede3ba
DOM6666-boom <vanndom300@gmail.com>
2025-02-06 23:31:48 +0700

    for my gf

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Crush",
+    introTitle: 'Kim Leang',
+    introDesc: `me sl u
+    sl me ot
+    u`,
+    btnIntro: '^^Click here^^',
+    title: 'Could it be that I’ve been in love with you from the very first moment I saw you? 🥰',
+    desc: 'Could it be that I fell for you the moment I saw that smile of yours?" 🥰',
+    btnYes: 'I’m in love with it <33',
+    btnNo: 'No chance :3',
+    question: 'There are over 7 billion people in the world, but why do you love me? <3',
+    btnReply: 'Sending this to you <3',
+    reply: 'If I love, I love; if I don’t love, I still love <33333333',
+    mess: 'I know 🥰. I love you so much 😘😘',
+    messDesc: 'Tonight at 7, I will come pick you up to hang out, okay?.',
+    btnAccept: 'Okayyy love <3',
+}
